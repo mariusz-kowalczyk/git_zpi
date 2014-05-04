e98b6b0782f1435ee172a305b2449d3d64cc3db2
Mariusz Kowalczyk <m.kowalczyk44446@gmail.com>
2014-05-04 13:04:10 +0200

    add entity framework

---
+++ git_zpi/git_zpi/Models/ZpiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_zpi.Models
+{
+    class ZpiDbContext : DbContext
+    {
+        public ZpiDbContext()
+            : base("SqlClient")
+        {
+
+        }
+    }
+}
