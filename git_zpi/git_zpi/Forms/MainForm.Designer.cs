e7b45609a8c7a931dd5cc607a4ac6de7e7d1776d
bradlol <rys1101@o2.pl>
2014-06-06 01:54:52 +0200

    pewnie nie bedzie dzialac
    i tak

--- git_zpi/git_zpi/Forms/MainForm.Designer.cs
+++ git_zpi/git_zpi/Forms/MainForm.Designer.cs
@@ -29,9 +29,22 @@
         private void InitializeComponent()
         {
             this.LogoutButton = new System.Windows.Forms.Button();
+            this.openFileDialogEdi = new System.Windows.Forms.OpenFileDialog();
             this.newUserButton = new System.Windows.Forms.Button();
             this.buttonGenerujFakture = new System.Windows.Forms.Button();
             this.usersListButton = new System.Windows.Forms.Button();
+            this.panelEdi = new System.Windows.Forms.Panel();
+            this.labelEdi = new System.Windows.Forms.Label();
+            this.buttonEdiFile = new System.Windows.Forms.Button();
+            this.panelKreskowe = new System.Windows.Forms.Panel();
+            this.textBoxBarcode = new System.Windows.Forms.TextBox();
+            this.pictureBoxKreskowe = new System.Windows.Forms.PictureBox();
+            this.buttonKreskowe = new System.Windows.Forms.Button();
+            this.comboBoxKreskowe = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelEdi.SuspendLayout();
+            this.panelKreskowe.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKreskowe)).BeginInit();
             this.SuspendLayout();
             // 
             // LogoutButton
@@ -43,6 +56,11 @@
             this.LogoutButton.Text = "Log out";
             this.LogoutButton.UseVisualStyleBackColor = true;
             this.LogoutButton.Click += new System.EventHandler(this.LogoutButton_Click);
+            // 
+            // openFileDialogEdi
+            // 
+            this.openFileDialogEdi.DefaultExt = "edi";
+            this.openFileDialogEdi.FileName = "openFileDialog1";
             // 
             // newUserButton
             // 
@@ -56,7 +74,7 @@
             // 
             // buttonGenerujFakture
             // 
-            this.buttonGenerujFakture.Location = new System.Drawing.Point(394, 264);
+            this.buttonGenerujFakture.Location = new System.Drawing.Point(533, 12);
             this.buttonGenerujFakture.Name = "buttonGenerujFakture";
             this.buttonGenerujFakture.Size = new System.Drawing.Size(121, 23);
             this.buttonGenerujFakture.TabIndex = 5;
@@ -74,17 +92,108 @@
             this.usersListButton.UseVisualStyleBackColor = true;
             this.usersListButton.Click += new System.EventHandler(this.usersListButton_Click);
             // 
+            // panelEdi
+            // 
+            this.panelEdi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEdi.Controls.Add(this.labelEdi);
+            this.panelEdi.Controls.Add(this.buttonEdiFile);
+            this.panelEdi.Location = new System.Drawing.Point(12, 13);
+            this.panelEdi.Name = "panelEdi";
+            this.panelEdi.Size = new System.Drawing.Size(230, 67);
+            this.panelEdi.TabIndex = 7;
+            // 
+            // labelEdi
+            // 
+            this.labelEdi.AutoSize = true;
+            this.labelEdi.Location = new System.Drawing.Point(3, 0);
+            this.labelEdi.Name = "labelEdi";
+            this.labelEdi.Size = new System.Drawing.Size(25, 13);
+            this.labelEdi.TabIndex = 0;
+            this.labelEdi.Text = "EDI";
+            // 
+            // buttonEdiFile
+            // 
+            this.buttonEdiFile.Location = new System.Drawing.Point(9, 30);
+            this.buttonEdiFile.Name = "buttonEdiFile";
+            this.buttonEdiFile.Size = new System.Drawing.Size(121, 23);
+            this.buttonEdiFile.TabIndex = 0;
+            this.buttonEdiFile.Text = "Edi z pliku";
+            this.buttonEdiFile.UseVisualStyleBackColor = true;
+            this.buttonEdiFile.Click += new System.EventHandler(this.buttonEdiFile_Click);
+            // 
+            // panelKreskowe
+            // 
+            this.panelKreskowe.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelKreskowe.Controls.Add(this.textBoxBarcode);
+            this.panelKreskowe.Controls.Add(this.pictureBoxKreskowe);
+            this.panelKreskowe.Controls.Add(this.buttonKreskowe);
+            this.panelKreskowe.Controls.Add(this.comboBoxKreskowe);
+            this.panelKreskowe.Controls.Add(this.label1);
+            this.panelKreskowe.Location = new System.Drawing.Point(12, 86);
+            this.panelKreskowe.Name = "panelKreskowe";
+            this.panelKreskowe.Size = new System.Drawing.Size(485, 138);
+            this.panelKreskowe.TabIndex = 8;
+            // 
+            // textBoxBarcode
+            // 
+            this.textBoxBarcode.Location = new System.Drawing.Point(9, 61);
+            this.textBoxBarcode.Name = "textBoxBarcode";
+            this.textBoxBarcode.Size = new System.Drawing.Size(121, 20);
+            this.textBoxBarcode.TabIndex = 4;
+            // 
+            // pictureBoxKreskowe
+            // 
+            this.pictureBoxKreskowe.Location = new System.Drawing.Point(157, 19);
+            this.pictureBoxKreskowe.Name = "pictureBoxKreskowe";
+            this.pictureBoxKreskowe.Size = new System.Drawing.Size(311, 101);
+            this.pictureBoxKreskowe.TabIndex = 3;
+            this.pictureBoxKreskowe.TabStop = false;
+            // 
+            // buttonKreskowe
+            // 
+            this.buttonKreskowe.Location = new System.Drawing.Point(9, 97);
+            this.buttonKreskowe.Name = "buttonKreskowe";
+            this.buttonKreskowe.Size = new System.Drawing.Size(121, 23);
+            this.buttonKreskowe.TabIndex = 2;
+            this.buttonKreskowe.Text = "Generuj Kod";
+            this.buttonKreskowe.UseVisualStyleBackColor = true;
+            this.buttonKreskowe.Click += new System.EventHandler(this.buttonKreskowe_Click);
+            // 
+            // comboBoxKreskowe
+            // 
+            this.comboBoxKreskowe.FormattingEnabled = true;
+            this.comboBoxKreskowe.Location = new System.Drawing.Point(9, 33);
+            this.comboBoxKreskowe.Name = "comboBoxKreskowe";
+            this.comboBoxKreskowe.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxKreskowe.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kody kreskowe";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(909, 551);
+            this.Controls.Add(this.panelKreskowe);
+            this.Controls.Add(this.panelEdi);
             this.Controls.Add(this.usersListButton);
             this.Controls.Add(this.buttonGenerujFakture);
             this.Controls.Add(this.newUserButton);
             this.Controls.Add(this.LogoutButton);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.panelEdi.ResumeLayout(false);
+            this.panelEdi.PerformLayout();
+            this.panelKreskowe.ResumeLayout(false);
+            this.panelKreskowe.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKreskowe)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -92,8 +201,18 @@
         #endregion
 
         private System.Windows.Forms.Button LogoutButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialogEdi;
         private System.Windows.Forms.Button newUserButton;
         private System.Windows.Forms.Button buttonGenerujFakture;
         private System.Windows.Forms.Button usersListButton;
+        private System.Windows.Forms.Panel panelEdi;
+        private System.Windows.Forms.Label labelEdi;
+        private System.Windows.Forms.Button buttonEdiFile;
+        private System.Windows.Forms.Panel panelKreskowe;
+        private System.Windows.Forms.TextBox textBoxBarcode;
+        private System.Windows.Forms.PictureBox pictureBoxKreskowe;
+        private System.Windows.Forms.Button buttonKreskowe;
+        private System.Windows.Forms.ComboBox comboBoxKreskowe;
+        private System.Windows.Forms.Label label1;
     }
 }
--- git_zpi/git_zpi/Forms/MainForm.cs
+++ git_zpi/git_zpi/Forms/MainForm.cs
@@ -1,22 +1,43 @@
 ﻿using git_zpi.Forms.Users;
 using git_zpi.Lib;
+using git_zpi.Library;
+using git_zpi.Library;
+using git_zpi.Models;
+using git_zpi.Repositories;
+using git_zpi.Library.Helper;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Zen.Barcode;
 
 namespace git_zpi.Forms
 {
     public partial class MainForm : BaseForm
     {
+        private IInvoiceRepository _invoice;
+        private List<Dict> Kody;
         public MainForm()
         {
             InitializeComponent();
+            _invoice = new InvoiceRepository(new ZpiDbContext());
+
+            Kody = new List<Dict>();
+            Kody.Add(new Dict("qr", "qr"));
+            Kody.Add(new Dict("code_11", "Code 11"));
+            Kody.Add(new Dict("code_39", "Code 39"));
+            Kody.Add(new Dict("code_128", "Code 128"));
+            Kody.Add(new Dict("ean8", "EAN-8"));
+            Kody.Add(new Dict("ean13", "EAN-13"));
+
+            comboBoxKreskowe.Items.AddRange(Kody.ToArray());
+            comboBoxKreskowe.SelectedIndex = 0;
         }
 
         private void LogoutButton_Click(object sender, EventArgs e)
@@ -39,6 +60,150 @@ namespace git_zpi.Forms
         private void usersListButton_Click(object sender, EventArgs e)
         {
             FormHelper.OpenForm(new ListUserForm());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cursor = Cursor;
+            Cursor = Cursors.WaitCursor;
+            string Edi = "UNH+1+INVOIC:D:96A:UN:EAN008'BGM+380+432097+9'DTM+137:19971008:102'NAD+SU+4012345500004::9++Firma ABC:::::+ul. Baby Jagi 1:::+Poznań++60–250+PL'RFF+VA:7770020410'NAD+BY+4012345500004::9++Firma ABC:::::+ul. Baby Jagi 1:::+Poznań++60–250+PL'RFF+VA:7770020410'NAD+DP+4012345500004::9++Firma ABCD:::::+ul. Baby Jagi 1:::+Poznań++60–250+PL'PAT+1'DTM+13:19970831:102'LIN+1++4000862141404:EU'IMD+C+name+RC::9:nazwa towaru/uslugi'QTY+47:40'MOA+66:580:PLN'PRI+AAA:14,50'TAX+7+VAT+++:::21+S'LIN+2++4000862141404:EU'IMD+C+name2+RC::9:nazwa towaru/uslugi'QTY+47:40'MOA+66:580:PLN'PRI+AAA:14,50'TAX+7+VAT+++:::21+S'UNS+S'CNT+2:120'MOA+77:45612,20'MOA+79:45612,20'MOA+124:45612,20'MOA+125:45612,20'TAX+7+VAT+++:::22+S'MOA+79:15243,32'MOA+124:3353,53'TAX+7+VAT+++:::21+S'UNT+84+1'";
+            InvoiceModel m = _invoice.EdiDecode(Edi);
+            _invoice.Add(m);
+            Cursor = cursor;
+        }
+
+        private void buttonEdiFile_Click(object sender, EventArgs e)
+        {
+            var cursor = Cursor;
+            Cursor = Cursors.WaitCursor;
+
+            openFileDialogEdi.ShowDialog();
+
+            string filename = openFileDialogEdi.FileName;
+            try
+            {
+                string content = File.ReadAllText(filename);
+
+
+                InvoiceModel m = _invoice.EdiDecode(content);
+                _invoice.Add(m);
+                MessageBox.Show("Wprowadzono pomyślnie plik EDI do bazy :)");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+
+
+            Cursor = cursor;
+        }
+
+
+        /*private void xmlButton_Click(object sender, EventArgs e)
+        {
+            if (xmlOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string xml = File.ReadAllText(xmlOpenFileDialog.FileName);
+                    InvoiceModel invoice = _invoice.XmlDecode(xml);
+                    try
+                    {
+                        _invoice.Add(invoice);
+                        MessageBox.Show("Zapisano z XML'a do bazy");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+        }*/
+
+        private void buttonKreskowe_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            Dict si = comboBoxKreskowe.SelectedItem as Dict;
+            string text = textBoxBarcode.Text;
+            Image barcodeImage = null;
+            switch (si.Key)
+            {
+                case "qr":
+                    CodeQrBarcodeDraw bd = BarcodeDrawFactory.CodeQr;
+                    barcodeImage = bd.Draw(text, 2, 2);
+                    break;
+                case "code_11":
+                    if (Barcode.isValidCode11(text))
+                    {
+                        Code11BarcodeDraw bd_code11 = BarcodeDrawFactory.Code11WithoutChecksum;
+                        barcodeImage = bd_code11.Draw(text, 60, 1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wprowadzona wartość nie może zostać wygenerowana.\nCode 11 zawiera cyfry od 0 do 9 oraz łącznik (-).");
+                    }
+                    break;
+                case "code_39":
+                    if (Barcode.isValidCode39(text))
+                    {
+                        Code39BarcodeDraw bd_code39 = BarcodeDrawFactory.Code39WithoutChecksum;
+                        barcodeImage = bd_code39.Draw(text, 60, 1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wprowadzona wartość nie może zostać wygenerowana.\n" +
+                            "Code 39 zawiera wszystkie litery, cyfry od 0 do 9, następujące znaki specjalne \"-.$/+%\" i spacje.");
+                    }
+                    break;
+                case "code_128":
+                    if (Barcode.isValidCode128(text))
+                    {
+                        Code128BarcodeDraw bd_code128 = BarcodeDrawFactory.Code128WithChecksum;
+                        barcodeImage = bd_code128.Draw(text, 60, 1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wprowadzona wartość nie może zostać wygenerowana.\n" +
+                            "");
+                    }
+                    break;
+                case "ean8":
+                    if (Barcode.isValidEan8(text))
+                    {
+                        CodeEan8BarcodeDraw bd_ean8 = BarcodeDrawFactory.CodeEan8WithChecksum;
+                        barcodeImage = bd_ean8.Draw(text, 60, 1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wprowadzona wartość nie może zostać wygenerowana.\n" +
+                            "Kod ten zawiera 8 cyfr, ale ostatni znak jest sumą kontrolną, więc należy wprowadzić 7 znaków.");
+                    }
+                    break;
+                case "ean13":
+                    if (Barcode.isValidEan13(text))
+                    {
+                        CodeEan13BarcodeDraw bd_ean13 = BarcodeDrawFactory.CodeEan13WithChecksum;
+                        barcodeImage = bd_ean13.Draw(text, 60, 1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wprowadzona wartość nie może zostać wygenerowana.\n" +
+                            "Kod ten zawiera 13 cyfr, ale ostatni znak jest sumą kontrolną, więc należy wprowadzić 12 znaków.");
+                    }
+                    break;
+            }
+            if (barcodeImage != null)
+            {
+                Print print = new Print();
+                print.printBarcode(barcodeImage, text);
+                pictureBoxKreskowe.Image = barcodeImage;
+            }
+
+            Cursor = Cursors.Default;
         }
     }
 }
